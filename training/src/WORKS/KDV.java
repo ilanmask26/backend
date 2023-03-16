ccfaff97795142dac59fd7f52084718b8d037c82
Furkan İlhan <122792308+ilanmask26@users.noreply.github.com>
2023-03-16 15:01:43 +0300

    Add files via upload

---
+++ training/src/WORKS/KDV.java
@@ -0,0 +1,24 @@
+
+
+package WORKS;
+
+import java.util.Scanner;
+public class KDV {
+    public static void main(String[] args) {
+        double price,tax,total;
+        // we appended the scanner variable
+        Scanner inp=new Scanner(System.in);
+
+        System.out.print("please enter your product price:");
+        price=inp.nextFloat();
+
+        tax= price>1000 ? (price*1.8/100):(price*18/100);
+        total=price+tax;
+
+        System.out.println("price:"+price);
+        System.out.println("total:"+total);
+        System.out.println("tax:"+tax);
+
+
+    }
+}
